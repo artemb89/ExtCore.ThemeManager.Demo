2a8d0e52ae0a713958b50fdde47259a7f8344e28
artemb89 <artem_b89@mail.ru>
2016-08-19 17:53:06 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ExtCore.ThemeManager.Demo.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {          
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExtCore.ThemeManager.Demo
+{
+    public class Startup: ExtCore.WebApplication.Startup
+    {
+        public Startup(IServiceProvider serviceProvider)
+      : base(serviceProvider)
+        {
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+              .SetBasePath(this.serviceProvider.GetService<IHostingEnvironment>().ContentRootPath)
+              .AddJsonFile("config.json", optional: true, reloadOnChange: true);
+
+            this.configurationRoot = configurationBuilder.Build();
+        }
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);         
+        }
+
+        public override void Configure(IApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.UseBrowserLink();
+            applicationBuilder.UseDeveloperExceptionPage();
+            applicationBuilder.UseDatabaseErrorPage();
+            
+            base.Configure(applicationBuilder);
+        }
+    }
+}
